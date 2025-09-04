314bcc53ee31e2860d8a3c07181f3e31d6a08906
Pranab Kumar Nath <65425483+npranab99@users.noreply.github.com>
2025-09-04 23:03:52 +0530

    Add files via upload
    first commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import AppRoutes from './appRoutes';
+import NavBar from './NavBar';
+
+
+function App() {
+  return (
+    <div className="App">
+    <NavBar />
+    <AppRoutes/>
+    </div>
+  );
+}
+
+export default App;
